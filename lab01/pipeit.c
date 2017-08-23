71b365d62de1771c093d6b019e001f140313c76b
Heriberto Rodriguez <hrodriguez1821@gmail.com>
2017-08-22 17:20:47 -0700

    changed documentation of pipe.c

--- lab01/pipeit.c
+++ lab01/pipeit.c
@@ -20,49 +20,47 @@ int main()
 	else if (pid == 0)
 	{
 		// child 1: sort -r > outfile
-		checkSysCall(close(fd[1]));
-		checkSysCall(dup2(fd[0], STDIN_FILENO));
-		checkSysCall(close(fd[0]));
+		checkSysCall(close(fd[1]));					/* child 1 will not write to pipe  */
+		checkSysCall(dup2(fd[0], STDIN_FILENO));	/* stdin now points to the read-end of the pipe */
+		checkSysCall(close(fd[0]));					/* file descriptor is not needed anymore */
 		
 		char *filename = "outfile";
 		int fw;
+
 		if ((fw = open("outfile", O_CREAT|O_RDWR)) == -1)
 		{
 			perror(filename);
 			exit(EXIT_FAILURE);
 		}
 
-		checkSysCall(dup2(fw, STDOUT_FILENO));
-		checkSysCall(close(fw));	
-		checkSysCall(execlp("sort", "sort", "-r", NULL));
+		checkSysCall(dup2(fw, STDOUT_FILENO));					/* stdout now points to file descriptor of outfile */
+		checkSysCall(close(fw));									/* file descriptor not needed anymore */
+		checkSysCall(execlp("sort", "sort", "-r", NULL));	/* executes sort -r > outfile */
 	}
-	else
+	
+	// parent: creates another child process
+	pid_t pid2;
+
+	if ((pid2 = fork()) < 0)
 	{
-		// parent: creates another child process
-		pid_t pid2;
-
-		if ((pid2 = fork()) < 0)
-		{
-			printf("fork failed\n");
-			return -1;
-		}
-		else if (pid2 == 0)
-		{
-			// child 2: ls
-			checkSysCall(close(fd[0]));						/* parent will not read from pipe */
-			checkSysCall(dup2(fd[1], STDOUT_FILENO));		/* stdout now points to the write-end of the pipe */
-			checkSysCall(close(fd[1]));						/* file descriptor is not needed anymore */
-			checkSysCall(execlp("ls", "ls", NULL));		/* execute ls */
-		}
+		printf("fork failed\n");
+		return -1;
+	}
+	else if (pid2 == 0)
+	{
+		// child 2: ls
+		checkSysCall(close(fd[0]));						/* child 2 will not read from pipe */
+		checkSysCall(dup2(fd[1], STDOUT_FILENO));		/* stdout now points to the write-end of the pipe */
+		checkSysCall(close(fd[1]));						/* file descriptor is not needed anymore */
+		checkSysCall(execlp("ls", "ls", NULL));		/* execute ls */
+	}
 		
-		checkSysCall(close(fd[0]));
-		checkSysCall(close(fd[1]));
+	checkSysCall(close(fd[0]));
+	checkSysCall(close(fd[1]));
 
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			printf("error with child 1\n");
-		}
-		printf("%d\n", status);
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		printf("error with child 1\n");
 	}
 
 	return 0;
