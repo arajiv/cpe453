e8b879f57a48fc9fc9a1226bf0983f0ff708dd85
Eri <eri@Brayans-MacBook-Air.local>
2017-09-18 17:43:27 -0700

    created a static library containing my malloc

---
+++ asgn1/asgn1.c
@@ -0,0 +1,8 @@
+#include "malloc.h"
+#include <stdio.h>
+
+int main()
+{
+	void *tmp = malloc(0);	
+	return 0;
+}
---
+++ asgn1/malloc.c
@@ -0,0 +1,8 @@
+#include "malloc.h"
+#include <stdio.h>
+
+void *malloc(size_t size)
+{
+	printf("in my malloc\n");
+	return NULL;
+}
